8b36fc45a90e1ed94c0c699b5001526c028dd896
Pierre <pierre@hubert.fr>
2017-09-24 16:53:24 +0200

    Can ask server to generate a login ticket

--- CentralAuth_library.php
+++ CentralAuth_library.php
@@ -1,11 +1,21 @@
 <?php
 /**
- * Central auth client library
+ * Central auth client library3
+ *
+ * This client is made to work with the first version
+ * of the API (V1)
  *
  * @author Pierre HUBERT
  */
 
 class CentralAuthClient {
+
+	/**
+	 * Server URL
+	 *
+	 * @var string
+	 */
+	private $server_url;
 
 	/**
 	 * Client ID
@@ -24,15 +34,119 @@ class CentralAuthClient {
 	/**
 	 * Public constructor
 	 *
+	 * @param stirng $server_url The URL of the CentralAuth Server
 	 * @param string $client_id The ID of the client application
 	 * @param string $client_secret The password of the client application
 	 */
-	public function __construct(string $client_id, string $client_secret){
+	public function __construct(string $server_url, string $client_id, string $client_secret){
+
+		//Save server URL
+		$this->server_url = $server_url;
 
 		//Store client ID and client Secret informations
 		$this->client_id = $client_id;
 		$this->client_secret = $client_secret;
 
+	}
+
+	/**
+	 * Create a new login ticket
+	 *
+	 * @param string $redirect_url The URL where CentralAuth should
+	 * redirect user once login is completed
+	 * @return array Informations about the newly created login ticket
+	 * * string login_ticket : The newly created login ticket
+	 * * string login_url : The URL where user should be redirected to perform login
+	 * * string error (optionnal): This is returned only in case of error
+	 */
+	public function create_login_ticket(string $redirect_url) : array {
+
+		//Perform a request on the server
+		$params = array(
+			//Redirect URL
+			"redirect_url" => $redirect_url,
+		);
+		$result = $this->post_request("create_ticket", $params);
+
+		//Check for errors
+		if($result['response_code'] !== 200 || isset($result['error']))
+			return array("error" => $result['response_code']);
+
+		print_r($result);
+
+	}
+
+	/**
+	 * Perform a POST request on the Central Auth Server
+	 *
+	 * @param string $uri The URI on the server of the request
+	 * @param array $params Parametres to include in the request
+	 * @return array The result
+	 */
+	private function post_request(string $uri, array $params = array()) : array {
+
+		//Generate the complete request URL
+		$request_url = $this->server_url."api/v1/".$uri;
+
+		//Append application token to the request
+		$params['client_id'] = $this->client_id;
+		$params['client_secret'] = $this->client_secret;
+
+		//Setup URL
+		$ch_opt[CURLOPT_URL] = $request_url;
+		$ch_opt[CURLOPT_HEADER] = FALSE;
+
+		//Perform a post request
+		$ch_opt[CURLOPT_POST] = TRUE;
+		$ch_opt[CURLOPT_POSTFIELDS] = $params;
+
+		//Generic config
+		$ch_opt[CURLOPT_RETURNTRANSFER] = TRUE;
+
+		//Setup cURL
+		$ch = curl_init();
+		curl_setopt_array($ch, $ch_opt);
+
+		//Execute URL
+		$response = curl_exec($ch);
+
+		//Check for errors
+		if(curl_errno($ch)){
+
+			//Retrieve error informations
+			$error = curl_error($ch);
+
+		}
+
+		//Check for server error
+		$response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+		//Close cURL
+		curl_close($ch);
+
+
+		//Process response
+		$result = array(
+			"response" => $response,
+			"response_code" => $response_code,
+		);
+
+		//Check for errors
+		if(isset($error))
+			$result['error'] = $error;
+
+		//Try to decode response
+		if(!isset($result['error'])){
+			//Decode JSON response
+			$response = json_decode($result['response'], true);
+
+			//Check if any error occured
+			if(count($response) > 0)
+				$result['response'] = $response;
+		}
+
+		//Return result
+		return $result;
 	}
 
 }
--- index.php
+++ index.php
@@ -5,7 +5,29 @@
  * @author Pierre HUBERT
  */
 
+//Start session
+session_start();
+
 require_once("CentralAuth_library.php");
 
 //Create Central Auth client object
-$caclient = new CentralAuthClient("CLIENT_ID", "CLIENT_SECRET");
+$caclient = new CentralAuthClient(
+	"http://devweb.local/centralAuth/project/",
+	"CLIENT_ID", 
+	"CLIENT_SECRET"
+);
+
+//Check for login ticket
+if(!isset($_SESSION['CA_login_ticket'])){
+
+	//Create login ticket
+	$redirect_url = $_SERVER['PHP_SELF']."?auth_key=%AUTHORIZATION%"; //Return URL
+	$login_ticket = $caclient->create_login_ticket($redirect_url);
+
+	//Check for errors
+	if(isset($login_ticket['error']))
+		exit("Couldn't generate a login ticket !");
+
+	print_r($login_ticket);
+
+}
